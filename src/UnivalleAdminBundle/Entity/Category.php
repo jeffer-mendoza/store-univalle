d920be57dc1d294df547614a89758d4dd873391a
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-03-11 15:28:49 -0500

    Se genera el bundle UnivalleAdminBundle, para realizar los cruds básicos de administración.

---
+++ src/UnivalleAdminBundle/Entity/Category.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace UnivalleAdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="UnivalleAdminBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+}
