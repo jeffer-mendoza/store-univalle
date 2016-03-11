25f9c07be057e4a9ebe2c712b96b177f6a4d31a0
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-03-11 16:51:57 -0500

    Creación del primer crud, que muestra la lista de entidades y una entidad individual(list y show)

---
+++ src/UnivalleAdminBundle/Controller/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace UnivalleAdminBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use UnivalleAdminBundle\Entity\Category;
+
+/**
+ * Category controller.
+ *
+ * @Route("/category")
+ */
+class CategoryController extends Controller
+{
+    /**
+     * Lists all Category entities.
+     *
+     * @Route("/", name="category_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('UnivalleAdminBundle:Category')->findAll();
+
+        return $this->render('category/index.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+
+    /**
+     * Finds and displays a Category entity.
+     *
+     * @Route("/{id}", name="category_show")
+     * @Method("GET")
+     */
+    public function showAction(Category $category)
+    {
+
+        return $this->render('category/show.html.twig', array(
+            'category' => $category,
+        ));
+    }
+
+
+}
